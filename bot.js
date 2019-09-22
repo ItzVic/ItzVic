47f29bc78689979f51d8dfa4e5639abe19f553e9
ItzVic <55650472+ItzVic@users.noreply.github.com>
2019-09-22 12:25:47 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,13 +3,14 @@ const bot = new Discord.Client();
 
 client.login("NTg3MjY4OTg4MzU5NDA5NjY1.XP0V7g.e5yUp1Q9EjOiFcvnYjcDiD4nMo0");
 
-const PREFIX = '-';
-
 client.on('ready', () =>{
     console.log('This bot is online!')
-})
+});
 
 client.on('message', message =>{
-let args = message.content.substring(PREFIX.length).split(" ");
-
-client.login(process.env.BOT_TOKEN);
+    if(message.author.bot)
+        return;
+    
+    if(message.content.toLowerCase() === 'hello')
+        message.channel.send("Hey!");
+});
